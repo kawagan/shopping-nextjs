43d261453905b7ae62985973002c8f1f26387bed
kawajan <kawajan2@gmail.com>
2025-09-14 11:33:36 +0200

    show products

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,16 @@
+import ProductList from '@/components/shared/header/product/product.list';
+import sampleData from '@/db/sample-data';
+
 const Home = async () => {
-  return <>Home</>;
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Featured Products"
+        limit={4}
+      />
+    </>
+  );
 };
 
 export default Home;
---
+++ components/shared/header/product/product.list.tsx
@@ -0,0 +1,23 @@
+const ProductList = ({
+  data,
+  title,
+  limit,
+}: {
+  data: any;
+  title: string;
+  limit?: number;
+}) => {
+  const limitData = limit ? data.slice(0, limit) : data;
+  return (
+    <div className="my-10">
+      <h2 className="h2-bold mb-4">{title}</h2>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  ">
+        {limitData.map((product) => (
+          <div key={product.name}>{product.name}</div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
