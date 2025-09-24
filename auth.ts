6eae254f780028cdbeb560126607543172b810ff
kawajan <kawajan2@gmail.com>
2025-09-24 16:44:48 +0200

    crate auth

---
+++ auth.ts
@@ -0,0 +1,59 @@
+import NextAuth from 'next-auth';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { prisma } from '@/db/prisma';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { compareSync } from 'bcryptjs';
+
+export const config = {
+  pages: {
+    signIn: '/auth/signin',
+    signOut: '/auth/signout',
+    error: '/auth/error', // Error code passed in query string as ?error=
+  },
+  session: {
+    strategy: 'jwt' as const,
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: {
+          type: 'email',
+        },
+        password: {
+          type: 'password',
+        },
+      },
+      async authorize(credentials) {
+        if (credentials == null) return null;
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+        if (user && user.password) {
+          const ismatch = compareSync(credentials.password, user.password);
+          if (ismatch) {
+            return {
+              id: user.id,
+              email: user.email,
+              name: user.name,
+              role: user.role,
+            };
+          }
+        }
+        return null;
+      },
+    }),
+  ],
+  callbacks: {
+    async session({ session, token, trigger, user }: any) {
+      session.user.id = token.sub;
+      if (trigger === 'update') {
+        session.user.name = user.name;
+      }
+      return session;
+    },
+  },
+};
+
+export const { handlers, auth, signIn, signOut } = NextAuth(config);
